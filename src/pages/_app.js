5cdc5bf78c4e7dfb73fd6d29d1928920c6a0fa12
Bengt Hagemeister <bengt@localticketing.de>
2023-09-29 12:22:50 +0200

    Added bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
+import { theme } from "../../theme";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
